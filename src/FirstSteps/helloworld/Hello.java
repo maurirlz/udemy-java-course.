8c3e4cbf671a4141f53fcb4d97a7a833d274e38e
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-02 19:32:15 -0300

    Added Variables excercise
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

--- src/FirstSteps/helloworld/Hello.java
+++ src/FirstSteps/helloworld/Hello.java
@@ -3,7 +3,7 @@ package FirstSteps.helloworld;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello. Mauri");
+        System.out.println("Hello, Mauri");
         //test commit and push
     }
 }
---
+++ src/Variables/Variables.java
@@ -0,0 +1,11 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int myFirstNumber = (10 + 5) + (2 * 10), mySecondNumber  = 12, myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+    }
+}

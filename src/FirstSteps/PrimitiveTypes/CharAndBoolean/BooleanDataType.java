cbd0fb4d6a6c7a3741888a707b78c8da8e6810a3
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-02 22:01:37 -0300

    Add More Data Types Exercises
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

---
+++ src/FirstSteps/PrimitiveTypes/CharAndBoolean/BooleanDataType.java
@@ -0,0 +1,13 @@
+package FirstSteps.PrimitiveTypes.CharAndBoolean;
+
+public class BooleanDataType {
+    public static void main(String[] args) {
+        // Boolean can have only two values: 1 or 0
+        boolean myTrueBooleanValue =  true;
+        boolean myFalseBooleanValue =  false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+
+    }
+}

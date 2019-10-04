0c72eab815e30d59b2e281ae5a0f421a86884fed
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-04 00:51:33 -0300

    Add CheckNumberExcercise
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

---
+++ src/ExpressionsStatementsCodeBlocksMethods/CheckNumberExcercise.java
@@ -0,0 +1,25 @@
+package ExpressionsStatementsCodeBlocksMethods;
+
+public class CheckNumberExcercise {
+    // Write a method called checkNumber with an int parameter number
+
+    // the method should not return any value, and it needs to print out:
+    /*- "positive" if the parameter number is > 0
+    - "negative" if the parameter number is < 0
+    - "zero" if the parameter number is equal to 0*/
+    /*Note: the checkNumber method needs to be defined in public static like we have been doing so far in the course.
+    Note: Do not add a main method to your solution code! */
+
+    public static void checkNumber(int number){
+        if (number > 0){
+            System.out.println("positive");
+
+        } else if (number < 0){
+            System.out.println("negative");
+
+        } else if (number == 0){
+            System.out.println("zero");
+
+        }
+    }
+}

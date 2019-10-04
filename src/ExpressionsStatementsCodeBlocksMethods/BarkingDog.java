3227dbc12dde2b9dd1a3cecc74f6e9961152d7ed
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-04 20:52:57 -0300

    Add multiple challenges done on the course
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

---
+++ src/ExpressionsStatementsCodeBlocksMethods/BarkingDog.java
@@ -0,0 +1,11 @@
+package ExpressionsStatementsCodeBlocksMethods;
+
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if (hourOfDay < 0 || hourOfDay > 23) {
+            return false;
+        } else {
+            return barking && (hourOfDay < 8 || hourOfDay > 22);
+        }
+    }
+}
---
+++ src/ExpressionsStatementsCodeBlocksMethods/MegaBytesConverter.java
@@ -0,0 +1,23 @@
+package ExpressionsStatementsCodeBlocksMethods;
+
+public class MegaBytesConverter {
+    public static void main(String[] args) {
+        int kiloBytes = 2500;
+        printMegaBytesAndKiloBytes(kiloBytes);
+
+    }
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if (kiloBytes < 0){
+            System.out.println("Invalid Value");
+        } else {
+            /* 1MB = 1024KB */
+            int megaBytes = kiloBytes / 1024;
+            int remainder = kiloBytes % 1024;
+
+            System.out.println(kiloBytes + " KB = "
+            + megaBytes + " MB and "
+            + remainder + " KB");
+
+        }
+    }
+}

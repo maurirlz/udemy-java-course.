5bb9514b9eae8d90b0354520f33587e2d5922a38
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-05 03:05:19 -0300

    Add exercises
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

---
+++ src/ExpressionsStatementsCodeBlocksMethods/AreaCalculator.java
@@ -0,0 +1,32 @@
+package ExpressionsStatementsCodeBlocksMethods;
+
+public class AreaCalculator {
+    public static void main(String[] args) {
+
+        double invalidCircleParameter = area(-1);
+        double validCircleParameter = area(5);
+
+        double invalidFirstRectanguleParameters = area(-1,5);
+        double invalidSecondRectanguleParameters = area(1,-5);
+
+        System.out.println("Area of the Circle: " + validCircleParameter);
+        System.out.println("Invalid parameter for Circle: " + invalidCircleParameter);
+
+        System.out.println("Area of the Rectangule: " + validCircleParameter);
+        System.out.println("Invalid first parameter for Rectangule: " + invalidFirstRectanguleParameters);
+        System.out.println("Invalid second parameter for Rectangule: " + invalidSecondRectanguleParameters);
+
+    }
+    public static double area(double radius){
+        if (radius < 0d){
+            return -1.0d;
+        }
+        return (Math.PI * (Math.pow(radius, 2d)));
+    }
+    public static double area(double x, double y){
+        if ((x < 0 ) || (y < 0)){
+            return -1.0d;
+        }
+        return (x * y);
+    }
+}

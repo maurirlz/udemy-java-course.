d68dd517cfd8ed13dfdfd9e2cd84a2d1a0285523
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-09 20:14:21 -0300

    Finish IntEqualityPrinter
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

---
+++ src/ExpressionsStatementsCodeBlocksMethods/IntEqualityPrinter.java
@@ -0,0 +1,20 @@
+package ExpressionsStatementsCodeBlocksMethods;
+
+public class IntEqualityPrinter {
+    public static void main(String[] args) {
+        printEqual(1,2,3);
+        printEqual(1,1,1);
+        printEqual(1,2,3);
+    }
+    public static void printEqual(int myFirstNumber, int mySecondNumber, int myThirdNumber) {
+        if (myFirstNumber < 0 || mySecondNumber < 0 || myThirdNumber < 0) {
+            System.out.println("Invalid Value");
+        } else if (myFirstNumber == myThirdNumber && mySecondNumber == myThirdNumber) {
+            System.out.println("All numbers are equal");
+        } else if (myFirstNumber != mySecondNumber && myFirstNumber != myThirdNumber && mySecondNumber  != myThirdNumber) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+}

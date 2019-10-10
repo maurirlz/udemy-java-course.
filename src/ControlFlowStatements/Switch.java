229cfbded2a953b7f0c6b3e6a96e67e71799d3fc
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-09 21:22:21 -0300

    Add Switch lecture and Challenge.
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

---
+++ src/ControlFlowStatements/Switch.java
@@ -0,0 +1,34 @@
+package ControlFlowStatements;
+
+public class Switch {
+    public static void main(String[] args) {
+        /*int value = 1;
+        if (value == 1) {
+            System.out.println("Value was 1");
+        } else if  (value == 2) {
+            System.out.println("value was 2");
+        } else  {
+            System.out.println("Was  not 1 or 2");
+        }*/
+
+
+        int switchValue = 4;
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3:case 4:case 5: // multiple cases.
+                System.out.println("Was a 3, 4, or 5");
+                System.out.println("Actually, it was a: " + switchValue);
+                break;
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
+        // more code here
+
+    }
+}
---
+++ src/ControlFlowStatements/SwitchChallenge.java
@@ -0,0 +1,49 @@
+package ControlFlowStatements;
+
+public class SwitchChallenge {
+    public static void main(String[] args) {
+        // Create a new switch statement using char instead of int
+        // create a new char variable
+        // create a switch statement testing for
+        // A, B, C, D, or E
+        // display a message if any of these are found and then break
+        // Add a default which displays a message saying not found
+        char charValue = 'A';
+
+        switch(charValue){
+            case 'A':
+                System.out.println("A was found.");
+                break;
+            case 'B':
+                System.out.println("B was found.");
+                break;
+            case 'C':
+                System.out.println("C was found.");
+                break;
+            case 'D':
+                System.out.println("D was found.");
+                break;
+            case 'E':
+                System.out.println("E was found.");
+                break;
+            default:
+                System.out.println("Neither of the cases were found.");
+                break;
+        }
+
+        String month = "january";
+
+        switch (month.toLowerCase()){
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("Not sure");
+                break;
+        }
+
+    }
+}

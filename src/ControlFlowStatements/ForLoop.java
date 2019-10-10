89512aa090f25b254ecce395478c90d0e3a5891e
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-10 02:04:29 -0300

    Add SumOf3And5Challenge exercise
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

--- src/ControlFlowStatements/ForLoop.java
+++ src/ControlFlowStatements/ForLoop.java
@@ -34,7 +34,7 @@ public class ForLoop {
 
             return false;
         }
-        for (int i = 2; i <= n/2; i++) {
+        for (int i = 2; i <= n / 2; i++) {
 
             if (n % i == 0) {
 
---
+++ src/ControlFlowStatements/SumOf3And5Challenge.java
@@ -0,0 +1,24 @@
+package ControlFlowStatements;
+
+public class SumOf3And5Challenge {
+    public static void main(String[] args) {
+        double acum = 0;
+        int cont = 0;
+
+        for (int i = 1; i <= 1000 ; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+
+                acum+=i;
+                cont++;
+                System.out.println("Number accumulated: " + i);
+                if (cont == 5) {
+
+                    break;
+                }
+            }
+        }
+
+        System.out.println("The sum of all numbers that made it into the acumulator is : " + acum);
+    }
+}

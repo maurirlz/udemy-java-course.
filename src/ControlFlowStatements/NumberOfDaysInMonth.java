64de98f00790f89d40d40537a783bca01cafd8a2
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-09 22:06:12 -0300

    Add NumberOfDaysInMonth Exercise.
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

---
+++ src/ControlFlowStatements/NumberOfDaysInMonth.java
@@ -0,0 +1,35 @@
+package ControlFlowStatements;
+
+public class NumberOfDaysInMonth {
+    public static void main(String[] args) {
+        System.out.println(getDaysInMonth(2,2020));
+
+    }
+
+    public static boolean isLeapYear(int year) {
+        if  (year >= 1 && year <= 9999){
+            return ((year % 400) == 0) || (((year % 100) != 0) && ((year % 4) == 0));
+        }
+        return false;
+    }
+
+    public static int getDaysInMonth(int month, int year){
+        if ((month >= 1 && month <= 12) && (year >= 1 && year <= 9999)) {
+            switch (month) {
+                case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                    return 31;
+                case 2:
+                    if (!isLeapYear(year)) {
+                        return 28;
+                    } else {
+                        return 29;
+                    }
+                case 4: case 6: case 9: case 11:
+                    return 30;
+                default:
+                    return -1;
+            }
+        }
+        return -1;
+    }
+}

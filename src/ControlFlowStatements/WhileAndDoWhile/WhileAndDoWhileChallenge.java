f52ba5531b42bccf4706ad1e81a2202b3979265e
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-10 17:55:06 -0300

    Add WhileAndDoWhile Challenge and course.
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

--- src/ControlFlowStatements/WhileAndDoWhile/WhileAndDoWhileChallenge.java
+++ src/ControlFlowStatements/WhileAndDoWhile/WhileAndDoWhileChallenge.java
@@ -5,17 +5,27 @@ public class WhileAndDoWhileChallenge {
 
         int number = 4;
         int finishNumber = 20;
+        int evennumbers = 0;
 
         while (number <= finishNumber) {
 
             number++;
+
             if (!isEvenNumber(number)) {
 
                 continue;
+            } else if (isEvenNumber(number)) {
+
+                evennumbers++;
+                if (evennumbers == 5) {
+
+                    break;
+                }
             }
 
             System.out.println("Even number found: " + number);
         }
+        System.out.println("Total of even numbers found: " + evennumbers);
     }
 
     public static boolean isEvenNumber(int number) {

2f35ed53ee4c71f50874d1b2c3c2486725f34f59
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-13 02:26:34 -0300

    Add Rectangle&Cuboid exercise.
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

---
+++ src/ObjectOrientedProgramming/ClassesObjectsConstructors/RectangleExercise/Cuboid.java
@@ -0,0 +1,25 @@
+package ObjectOrientedProgramming.ClassesObjectsConstructors.RectangleExercise;
+
+public class Cuboid extends Rectangle {
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+
+        if (height < 0) {
+
+            height = 0;
+        }
+
+        this.height = height;
+    }
+
+    public double getVolume() {
+        return (getArea() * this.height);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+}
---
+++ src/ObjectOrientedProgramming/ClassesObjectsConstructors/RectangleExercise/Rectangle.java
@@ -0,0 +1,33 @@
+package ObjectOrientedProgramming.ClassesObjectsConstructors.RectangleExercise;
+
+public class Rectangle {
+
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+
+        if (width < 0) {
+            width = 0;
+        }
+
+        if (length < 0) {
+            length = 0;
+        }
+
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getArea() {
+        return (this.length * this.width);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+}

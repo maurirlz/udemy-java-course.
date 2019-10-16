3ba774e708b09df2e8e2b2054310391a4564f039
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-15 22:25:11 -0300

    Finish composition challenge.
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

--- src/ObjectOrientedProgramming/Composition/Challenge/Room.java
+++ src/ObjectOrientedProgramming/Composition/Challenge/Room.java
@@ -4,18 +4,25 @@ public class Room {
 
     private Door door;
     private Window window;
-    private Closet closet;
     private AirConditioner airconditioner;
     private Bed bed;
     private Shelf shelf;
 
-    public Room(Door door, Window window, Closet closet, AirConditioner airconditioner, Bed bed, Shelf shelf) {
+    public Room(Door door, Window window, AirConditioner airconditioner, Bed bed, Shelf shelf) {
         this.door = door;
         this.window = window;
-        this.closet = closet;
         this.airconditioner = airconditioner;
         this.bed = bed;
         this.shelf = shelf;
+    }
+
+    private boolean isShelfSafe() {
+        if (shelf.getNumberOfScrews() < 6 && shelf.getLimitOnKilograms() > 30d) {
+            return false;
+        } else {
+            System.out.println("The shelf is safe.");
+            return true;
+        }
     }
 
     private void isRoomLocked() {
@@ -33,4 +40,55 @@ public class Room {
         }
     }
 
+    public Door getDoor() {
+        return door;
+    }
+
+    public Window getWindow() {
+        return window;
+    }
+
+    public AirConditioner getAirconditioner() {
+        return airconditioner;
+    }
+
+    public Bed getBed() {
+        return bed;
+    }
+
+    public Shelf getShelf() {
+        return shelf;
+    }
+
+    public void setDoor(Door door) {
+        this.door = door;
+    }
+
+    public void setWindow(Window window) {
+        this.window = window;
+    }
+
+    public void setAirconditioner(AirConditioner airconditioner) {
+        this.airconditioner = airconditioner;
+    }
+
+    public void setBed(Bed bed) {
+        this.bed = bed;
+    }
+
+    public void setShelf(Shelf shelf) {
+        this.shelf = shelf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Room)) return false;
+        Room room = (Room) o;
+        return door.equals(room.door) &&
+                window.equals(room.window) &&
+                airconditioner.equals(room.airconditioner) &&
+                bed.equals(room.bed) &&
+                shelf.equals(room.shelf);
+    }
 }
--- src/ObjectOrientedProgramming/Composition/Challenge/Shelf.java
+++ src/ObjectOrientedProgramming/Composition/Challenge/Shelf.java
@@ -1,4 +1,38 @@
 package ObjectOrientedProgramming.Composition.Challenge;
 
 public class Shelf {
+
+    private String material;
+    private int numberOfScrews;
+    private double limitOnKilograms;
+
+    public Shelf(String material, int numberOfScrews, double limitOnKilograms) {
+        this.material = material;
+        this.numberOfScrews = numberOfScrews;
+        this.limitOnKilograms = limitOnKilograms;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public int getNumberOfScrews() {
+        return numberOfScrews;
+    }
+
+    public double getLimitOnKilograms() {
+        return limitOnKilograms;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public void setNumberOfScrews(int numberOfScrews) {
+        this.numberOfScrews = numberOfScrews;
+    }
+
+    public void setLimitOnKilograms(double limitOnKilograms) {
+        this.limitOnKilograms = limitOnKilograms;
+    }
 }

a980cf1db030e3c965890255fb63d3c63a7d5af5
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2019-10-19 22:50:35 -0300

    Finish minimum element challenge.
    Signed-off-by: Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>

---
+++ src/ArraysJavaListsAutoboxingUnboxing/Arrays/MinimumElementChallenge.java
@@ -0,0 +1,57 @@
+package ArraysJavaListsAutoboxingUnboxing.Arrays;
+
+import java.util.Scanner;
+
+public class MinimumElementChallenge {
+
+    private static Scanner numSc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] intArray = new int[5];
+        int minElement;
+
+        intArray = readIntegers(intArray.length);
+
+        minElement = findMin(intArray);
+        System.out.println(minElement);
+    }
+
+
+    public static int findMin(int[] array) {
+        int min = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0) {
+
+                min = array[i];
+            } else {
+                if (array[i] < min) {
+
+                    min = array[i];
+                }
+            }
+        }
+        return min;
+    }
+
+    public static int[] readIntegers(int dimension) {
+
+        int[] intArray = new int[dimension];
+
+        for (int i = 0; i < intArray.length; i++) {
+
+            System.out.println("Please, enter a Integer to be stored in index " + (i + 1) + "\r");
+
+            if (numSc.hasNextInt()) {
+
+                intArray[i] = numSc.nextInt();
+            } else {
+                System.out.println("Invalid value, please input an integer.");
+                i--;
+            }
+         }
+
+        return intArray;
+    }
+
+}

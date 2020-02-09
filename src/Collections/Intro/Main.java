4436ef49c445b75d878876546b43e39cbeeb8326
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2020-02-09 05:05:03 -0300

    Finish lesson on various methods for list in Collections.

--- src/Collections/Intro/Main.java
+++ src/Collections/Intro/Main.java
@@ -1,10 +1,43 @@
 package Collections.Intro;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+        Theatre hoytsCinema = new Theatre("Hoyts", 8, 12);
 
-        Theatre hoytsTheatre = new Theatre("Hoyts", 8, 12);
-        hoytsTheatre.getSeats();
+        // Coping an an Arraylist to another Arraylist.
+
+        List<Theatre.Seat> seatCopy = new ArrayList<>(hoytsCinema.seats);
+
+        // Usage of reverse() and shuffle():
+
+//        Collections.reverse(seatCopy);
+//        System.out.println("Printing seatCopy");
+//        printList(seatCopy);
+//        System.out.println("Printing Hoyts Cinema arraylist in shuffle order.");
+//        Collections.shuffle(hoytsCinema.seats);
+//        printList(hoytsCinema.seats);
+
+        // usage of min() and max():
+        Theatre.Seat lesserSeat = Collections.min(seatCopy);
+        Theatre.Seat higherSeat = Collections.max(seatCopy);
+
+        System.out.println("The higher number of seats available is: " + higherSeat.getSeatNumber());
+        System.out.println("The lesser number of seats available is: " + lesserSeat.getSeatNumber());
+
+
+    }
+
+    public static void printList(List<Theatre.Seat> list) {
+        for (Theatre.Seat seat : list) {
+
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("============================================================================");
     }
 }

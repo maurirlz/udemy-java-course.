a15cc2aaeda518286a8c542a818a56ee18f70c48
maurirlz <mauribenitez95@gmail.com>
2020-02-11 08:05:00 -0300

    finish Comparator and Comparable lessons.

--- src/Collections/Intro/Main.java
+++ src/Collections/Intro/Main.java
@@ -2,56 +2,41 @@ package Collections.Intro;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Theatre hoytsCinema = new Theatre("Hoyts", 8, 12);
 
-        // Coping an an Arraylist to another Arraylist.
-
-        List<Theatre.Seat> seatCopy = new ArrayList<>(hoytsCinema.seats);
-
-        // Usage of reverse() and shuffle():
-
-//        Collections.reverse(seatCopy);
-//        System.out.println("Printing seatCopy");
-//        printList(seatCopy);
-//        System.out.println("Printing Hoyts Cinema arraylist in shuffle order.");
-//        Collections.shuffle(hoytsCinema.seats);
-//        printList(hoytsCinema.seats);
-
-        // usage of min() and max():
-        Theatre.Seat lesserSeat = Collections.min(seatCopy);
-        Theatre.Seat higherSeat = Collections.max(seatCopy);
-
-        System.out.println("The higher number of seats available is: " + higherSeat.getSeatNumber());
-        System.out.println("The lesser number of seats available is: " + lesserSeat.getSeatNumber());
+        Theatre hoysCinema = new Theatre("Hoyts", 8, 12);
+        List<Theatre.Seat> seatCopy = new ArrayList<>();
 
 
+        if (hoysCinema.reserveSeat("A02")) {
+
+            System.out.println("Please pay for A02");
+        } else {
+
+            System.out.println("Seat already reserved.");
+        }
+
+        List<Theatre.Seat> priceSeats = new ArrayList<>(hoysCinema.getSeats());
+        priceSeats.add(hoysCinema.new Seat("B00", 13.00));
+        priceSeats.add(hoysCinema.new Seat("A00", 13.00));
+        Collections.sort(priceSeats, Theatre.PRICE_ORDER);
+
+        printList(priceSeats);
     }
 
     public static void printList(List<Theatre.Seat> list) {
         for (Theatre.Seat seat : list) {
 
-            System.out.print(" " + seat.getSeatNumber());
+            System.out.print(" " + seat.getSeatNumber() + " " + seat.getPrice());
         }
         System.out.println();
         System.out.println("============================================================================");
     }
 
-    public static void sortList(List<? extends Theatre.Seat> list) {
-
-        for (int i = 0; i < list.size(); i++) {
-
-            for (int j = i + 1; j < list.size(); j++) {
-
-                if (list.get(i).compareTo(list.get(j)) > 0) {
-
-                    Collections.swap(list, i, j);
-                }
-            }
-        }
-    }
 }
+

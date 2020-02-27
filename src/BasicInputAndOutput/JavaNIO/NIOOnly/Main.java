d2f647774f1dcc07247a557c361183f250493afa
maaush <mauribenitez95@gmail.com>
2020-02-27 17:52:10 -0300

    Finish Writing and Reading data from a file using pure Java.NIO

---
+++ src/BasicInputAndOutput/JavaNIO/NIOOnly/Main.java
@@ -0,0 +1,33 @@
+package BasicInputAndOutput.JavaNIO.NIOOnly;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Path dataPath = FileSystems.getDefault().getPath("data.txt");
+            Files.write(dataPath, "\nLine 5".getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+            List<String> lines = Files.readAllLines(dataPath);
+
+            for (String line : lines) {
+
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}

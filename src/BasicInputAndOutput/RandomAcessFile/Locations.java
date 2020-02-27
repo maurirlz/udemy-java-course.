4072e27debc480f98489d0826bd1a35119550a9d
maaush <mauribenitez95@gmail.com>
2020-02-27 17:12:11 -0300

    delete comments on Random access file lesson

--- src/BasicInputAndOutput/RandomAcessFile/Locations.java
+++ src/BasicInputAndOutput/RandomAcessFile/Locations.java
@@ -16,49 +16,7 @@ public class Locations implements Map<Integer, Location> {
     private static RandomAccessFile ra;
 
     public static void main(String[] args) throws IOException {
-
-        // Character writing using FileWriter and BufferedWriter
-
-//        try (BufferedWriter locFile = new BufferedWriter(new FileWriter("locations.txt"));
-//            BufferedWriter dirFile = new BufferedWriter(new FileWriter("directions.txt"))) {
-//
-//            for (Location location : locations.values()) {
-//
-//                locFile.write(location.getLocationID() + " - " + location.getDescription() + "\n");
-//                for (String direction : location.getExits().keySet()) {
-//
-//                    if (!direction.equalsIgnoreCase("q")) {
-//
-//                        dirFile.write(location.getLocationID() + " - " + direction + " - " + location.getExits().get(direction) + "\n");
-//                    }
-//                }
-//            }
-//        }
-
-        // Byte stream wiriting using DataOutputStream with BufferedOutputStream and FileOutputStream
-
-//        try (DataOutputStream locFIle = new DataOutputStream(new BufferedOutputStream(new FileOutputStream("locations.dat")))) {
-//
-//
-//            for (Location location : locations.values()) {
-//
-//                locFIle.writeInt(location.getLocationID());
-//                locFIle.writeUTF(location.getDescription());
-//                System.out.println("writing location " + location.getLocationID() + " : " + location.getDescription());
-//                System.out.println("Writing " + (location.getExits().size() - 1) + " exits");
-//                locFIle.writeInt(location.getExits().size() - 1);
-//                for (String direction : location.getExits().keySet()) {
-//
-//                    if (!direction.equalsIgnoreCase("q")) {
-//
-//                        System.out.println("\t\t" + direction + "," + location.getExits().get(direction));
-//                        locFIle.writeUTF(direction);
-//                        locFIle.writeInt(location.getExits().get(direction));
-//                    }
-//                }
-//            }
-//        }
-
+        
         try (RandomAccessFile rao = new RandomAccessFile("locations_rand.dat", "rwd")) {
 
             rao.writeInt(locations.size()); // point 1

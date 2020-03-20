1b5ce9cdaa770baa21c0df83ff00d4bcf5582072
maaush <mauribenitez95@gmail.com>
2020-03-20 00:40:00 -0300

    Finish Random access writing and reading using Channels and Buffers with Java.NIO.

--- src/BasicInputAndOutput/JavaNIO/NIOOnly/BinaryNIOReadingAndWriting.java
+++ src/BasicInputAndOutput/JavaNIO/NIOOnly/BinaryNIOReadingAndWriting.java
@@ -16,58 +16,130 @@ public class BinaryNIOReadingAndWriting {
         try (FileOutputStream binFile = new FileOutputStream(file)) {
 
             FileChannel binChannel = binFile.getChannel();
+
+            /* Writing and Reading everything into a buffer in one shot into the buffer
+            then writing the channel with that buffer.:  */
+
+            ByteBuffer buffer = ByteBuffer.allocate(100);
+
+
+            /* Chained put method calls */
+
+//            byte[] outputBytes = "Hello World!".getBytes();
+//            byte[] outputBytes2 = "Nice to meet you".getBytes();
+//            buffer.put(outputBytes).putInt(245).putInt(-98765).put(outputBytes2).putInt(1002).flip();
+
+            /*  Unchained put methods calls, and wiriting data in backwards
+            * order, using methods from SeekeableByteChannel */
+
             byte[] outputBytes = "Hello World!".getBytes();
-            ByteBuffer buffer = ByteBuffer.allocate(outputBytes.length);
             buffer.put(outputBytes);
+            long ini1Pos = outputBytes.length;
+
+            buffer.putInt(245);
+            long int2Pos = ini1Pos + Integer.BYTES;
+
+            buffer.putInt(-98765);
+            byte[] outputBytes2 = "Nice to meet you".getBytes();
+            buffer.put(outputBytes2);
+            long int3Pos = int2Pos + Integer.BYTES + outputBytes2.length;
+
+            buffer.putInt(1000);
             buffer.flip();
-            int numBytes = binChannel.write(buffer);
+            binChannel.write(buffer);
 
-            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-            intBuffer.putInt(245);
-            intBuffer.flip();
-            binChannel.write(intBuffer);
+            /* Reading data backwards using indexes with SeekeableByteChannel interface  */
 
-            System.out.println("numBytes written was: " + numBytes);
-
-            /* Reading data with a RAF using java.io package. */
-
-//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-//            byte[] byteArray = new byte[outputBytes.length];
-//            ra.read(byteArray);
-//            System.out.println(new String(byteArray));
-//
-//            long firstInt = ra.readInt();
-//            System.out.println(firstInt);
-
-            /* Reading data using a RAF with Java.NIO */
-            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            RandomAccessFile ra = new RandomAccessFile(file, "rwd");
             FileChannel channel = ra.getChannel();
+            ByteBuffer readBuffer = ByteBuffer.allocate(Integer.BYTES);
 
-            buffer.flip();
-            long numBytesRead = channel.read(buffer);
-//            System.out.println("outputBytes = " + new String(outputBytes));
+            long[] positions = new long[3];
 
-            if (buffer.hasArray()) {
+            positions[2] = int3Pos;
+            positions[1] = int2Pos;
+            positions[0] = ini1Pos;
 
-                System.out.println("bytebuffer = " + new String(buffer.array()));
+            for (int i = positions.length - 1; i >= 0; i--) {
+
+                channel.position(positions[i]);
+                channel.read(readBuffer);
+                readBuffer.flip();
+
+                System.out.println(" Number read: " + readBuffer.getInt());
+
+                if (i != 0) {
+
+                    readBuffer.flip();
+                }
             }
 
-            /* Relative reads of Integers */
+
+            /* Reading data sequentally.*/
+
+//            byte[] inputString = new byte[outputBytes.length];
+//            readBuffer.get(inputString);
+//            System.out.println("inputString = " + new String(inputString));
+//
+//            System.out.println("int 1 = " + readBuffer.getInt());
+//            System.out.println("int 2 = " + readBuffer.getInt());
+//
+//            byte[] inputString2 = new byte[outputBytes2.length];
+//            readBuffer.get(inputString2);
+//
+//            System.out.println("inputString2 = " + new String(inputString2));
+//            System.out.println("int 3 = " + readBuffer.getInt());
+
+            /* Writing and Reading everything into a buffer sequentally*/
+//            ByteBuffer buffer = ByteBuffer.allocate(outputBytes.length);
+//            buffer.put(outputBytes);
+//            buffer.flip();
+//            int numBytes = binChannel.write(buffer);
+//
+//            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
+//            intBuffer.putInt(245);
+//            intBuffer.flip();
+//            binChannel.write(intBuffer);
+//
+//            System.out.println("numBytes written was: " + numBytes);
+//
+//            /* Reading data with a RAF using java.io package. */
+//
+////            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+////            byte[] byteArray = new byte[outputBytes.length];
+////            ra.read(byteArray);
+////            System.out.println(new String(byteArray));
+////
+////            long firstInt = ra.readInt();
+////            System.out.println(firstInt);
+//
+//            /* Reading data using a RAF with Java.NIO */
+//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+//            FileChannel channel = ra.getChannel();
+//
+//            buffer.flip();
+//            long numBytesRead = channel.read(buffer);
+////            System.out.println("outputBytes = " + new String(outputBytes));
+//
+//            if (buffer.hasArray()) {
+//
+//                System.out.println("bytebuffer = " + new String(buffer.array()));
+//            }
+//
+//            /* Relative reads of Integers */
+////            intBuffer.flip();
+////            numBytesRead = channel.read(intBuffer);
+////            intBuffer.flip();
+////            System.out.println(intBuffer.getInt());
+////            channel.close();
+////            ra.close();
+//
+//            /* Absolute read of Integers, passing an argument to the getInt method to point where exactly the read should begin. */
+//
 //            intBuffer.flip();
 //            numBytesRead = channel.read(intBuffer);
+//            System.out.println(intBuffer.getInt(0));
 //            intBuffer.flip();
-//            System.out.println(intBuffer.getInt());
-//            channel.close();
-//            ra.close();
-
-            /* Absolute read of Integers, passing an argument to the getInt method to point where exactly the read should begin. */
-
-            intBuffer.flip();
-            numBytesRead = channel.read(intBuffer);
-            System.out.println(intBuffer.getInt(0));
-            intBuffer.flip();
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }

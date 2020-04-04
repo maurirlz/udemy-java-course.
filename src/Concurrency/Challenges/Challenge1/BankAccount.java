c95a0b33c8f9b40773294bcbc9ccff52d354b564
maaush <mauribenitez95@gmail.com>
2020-04-04 02:47:29 -0300

    Finish concurrency section of the course.

--- src/Concurrency/Challenges/Challenge1/BankAccount.java
+++ src/Concurrency/Challenges/Challenge1/BankAccount.java
@@ -1,30 +1,84 @@
 package Concurrency.Challenges.Challenge1;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
 
     private double balance;
     private String accountNumber;
+    private ReentrantLock lock;
 
     public BankAccount(String accountNumber, double initialBalance) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        this.lock = new ReentrantLock();
     }
 
     public void deposit(double amount) {
-        synchronized (this) {
 
-            balance += amount;
+        boolean status = false;
+
+        try {
+
+            if (lock.tryLock(1000L, TimeUnit.MILLISECONDS)) {
+
+                try {
+
+                    balance += amount;
+                    status = true;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+
+                System.out.println("Couldn't get the lock.");
+            }
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
         }
+
+        System.out.println("Status: " + status);
     }
 
     public void withdraw(double amount) {
-        synchronized (this) {
 
-            balance -= amount;
+        boolean status = false;
+        try {
+
+            if (lock.tryLock(1000L, TimeUnit.MILLISECONDS)) {
+
+                try {
+                    balance -= amount;
+                    status = true;
+                } finally {
+
+                    lock.unlock();
+                }
+            } else {
+
+                System.out.println("Couldn't get the lock.");
+            }
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
         }
+
+        System.out.println("Status: " + status);
     }
 
     public double getBalance() {
         return balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void printAccountNumber() {
+
+
+        System.out.println("Account number: " + accountNumber);
     }
 }

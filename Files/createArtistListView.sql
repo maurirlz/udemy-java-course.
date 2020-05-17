b4257883ecc4d3fb8d212c37cca1ee0081f4b916
Mauricio Ezequiel Benitez <49231442+maurirlz@users.noreply.github.com>
2020-05-17 16:47:23 -0300

    Update createArtistListView.sql

--- Files/createArtistListView.sql
+++ Files/createArtistListView.sql
@@ -1,4 +1,4 @@
-CREATE VIEW artist_list AS
+CREATE VIEW IF NOT EXISTS artist_list AS
 SELECT artists.name AS artist, albums.name AS album, songs.track, songs.title FROM songs 
 INNER JOIN albums ON songs.album = albums._id
 INNER JOIN artists ON albums.artist = artists._id

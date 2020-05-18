411d86cb1a14e6a22a2d42e1332ab81d6fd889a7
maaush <mauribenitez95@gmail.com>
2020-05-18 00:58:59 -0300

    Started Networking lessons, finished TCP/IP protocols exercise, making a Server and multiple clients that connect to the former.

---
+++ src/Networking/TCPIP/EchoClient/Main.java
@@ -0,0 +1,48 @@
+package Networking.TCPIP.EchoClient;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        try (Socket socket = new Socket("localhost", 5000)) {
+
+            socket.setSoTimeout(5000);
+
+            BufferedReader echoes = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
+
+            String echoString;
+            String response;
+
+            do {
+
+                System.out.println("Enter string to be echoed: ");
+                echoString = sc.nextLine();
+
+                stringToEcho.println(echoString);
+
+                if (!echoString.equalsIgnoreCase("exit")) {
+                    response = echoes.readLine();
+                    System.out.println(response);
+                }
+
+            } while (!echoString.equalsIgnoreCase("exit"));
+
+        } catch (SocketTimeoutException e) {
+
+            System.out.println("socket timed out. " +  e.getMessage());
+        } catch (IOException e) {
+
+            System.out.println("Client error: " + e.getMessage());
+        }
+    }
+}
---
+++ src/Networking/TCPIP/EchoServer/Echoer.java
@@ -0,0 +1,47 @@
+package Networking.TCPIP.EchoServer;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Echoer extends Thread {
+
+    private final Socket socket;
+
+    public Echoer(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+            while (true) {
+                String echoString = reader.readLine();
+                System.out.println("Recieved client input: " + echoString);
+
+                if (echoString.equalsIgnoreCase("exit")) {
+
+                    break;
+                }
+
+                writer.println(echoString);
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("Oops. " + e.getMessage());
+        } finally {
+            try {
+
+                socket.close();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+        }
+    }
+}
